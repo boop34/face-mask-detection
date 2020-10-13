073c5d62051f0f55aa76b53a2e73267307fff079
Subhadeep Deb <subhadeepdeb34@gmail.com>
2020-10-14 01:23:07 +0530

    updated for command line usage

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # set up the face detector model
 fmodel = cv2.dnn.readNetFromDarknet('model-weights/yolov3-face.cfg',
-                              'model-weights/yolov3-wider_16000.weights')
+                                    'model-weights/yolov3-wider_16000.weights')
 
 # set up the mask detector model
 model = load_model('model.h5')
@@ -21,9 +21,12 @@ model = load_model('model.h5')
 labels = ['cwm', 'iwm', 'nwm']
 
 # set up a few helper function
+
+
 def get_outputs_names(net):
     layers_names = net.getLayerNames()
     return [layers_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
 
 # function to extract the faces with most confidence
 def post_process(frame, outs, conf_threshold, nms_threshold):
@@ -66,6 +69,8 @@ def post_process(frame, outs, conf_threshold, nms_threshold):
         final_boxes.append(box)
     return final_boxes
 
+
+# make the boxes a little bit bigger
 def refined_box(left, top, width, height):
     right = left + width
     bottom = top + height
@@ -76,69 +81,109 @@ def refined_box(left, top, width, height):
 
     margin = ((bottom - top) - (right - left)) // 2
     left = left - margin if (bottom - top - right + left) % 2 == 0 \
-           else left - margin - 1
+        else left - margin - 1
 
     right = right + margin
 
     return left, top, right, bottom
 
+
+# process image to feed it to model
 def post_process_image(img):
     img = img_to_array(img)
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
     return img
 
-@app.route('/detect', methods=['POST'])
-def getData():
-    # retrive the information sent
-    recieved_data = request.form
+
+# detect faces and labels
+def detect_faces(imgEnc):
     # get the images string
-    imgStr = base64.b64decode(recieved_data['imgB'].encode('utf-8'))
-    # get the extension
-    ext = recieved_data['ext']
+    imgStr = base64.b64decode(imgEnc.encode('utf-8'))
+
     # turn the image string into numpy array
     nparr = np.frombuffer(imgStr, np.uint8)
     # turn the numpy array into image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
     # create a blob from the data
-    blob = cv2.dnn.blobFromImage(image=img, scalefactor=1/255,
+    blob = cv2.dnn.blobFromImage(image=img, scalefactor=1 / 255,
                                  size=(416, 416), mean=[0, 0, 0],
                                  swapRB=1, crop=False)
+
     # set the blob as the imput to the face detector model
     fmodel.setInput(blob)
     # do a forward pass to get all the possible faces
     outs = fmodel.forward(get_outputs_names(fmodel))
     # get all the faces with most confidence
     faces = post_process(img, outs, 0.5, 0.4)
+
     # draw the rectangle(s)
     for (x, y, w, h) in faces:
         # get the left, top, right, bottom coordinates
         left, top, right, bottom = refined_box(x, y, w, h)
+
         # define some colors
         colRed = (255, 0, 0)
         colYellow = (0, 255, 255)
         colGreen = (0, 255, 255)
         colWhite = (255, 255, 255)
+
+        # extract the face from the image
         tempImg = img[top:bottom, left:right]
+        # resize it to 224x224 and process it to fit to the model
         tempImg = cv2.resize(tempImg, (224, 224))
         tempImg = post_process_image(tempImg)
+
+        # get the prediction
         prediction = model.predict(tempImg)
+        # determine the label
         label = labels[np.argmax(prediction)]
+        # get the confidence from the prediction
         conf = prediction[label]
+
+        # set up the color accotding to the prediction
+        if label == 'cwm':
+            col = colGreen
+        elif label == 'iwm':
+            col = colYellow
+        else:
+            col = colRed
+
+        # form the text to draw
         text = f'{label}-{conf: .3f}%'
+        # set up the text
         label_size, base_line = cv2.getTextSize(text,
                                                 cv2.FONT_HERSHEY_SIMPLEX,
                                                 0.5, 1)
-        img = cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 2)
+        # draw the rectangle
+        img = cv2.rectangle(img, (left, top), (right, bottom), col, 2)
         top = max(top, label_size[1])
-        img = cv2.putText(img, text, (left, top - 4),cv2.FONT_HERSHEY_SIMPLEX,
+        # draw the text
+        img = cv2.putText(img, text, (left, top - 4), cv2.FONT_HERSHEY_SIMPLEX,
                           0.4, colWhite, 1)
+
     # convert the image to show it via PIL on the client end
     tempImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # convert it to list to send to client and return it
+    return tempImg.tolist()
+
+
+@app.route('/detect', methods=['POST'])
+def getData():
+    # get the list of images
+    imgList = request.form['imgB']
+
+    # set up the response list
+    resList = []
+    for (imgEnc, fname, ext) in imgList:
+        resImg = detect_faces(imgEnc)
+        resList.append((resImg, fname, ext))
     # setup the responce body
-    res = {'detected_faces': tempImg.tolist()}
+    res = {'detected_faces': resList}
     # return the response
     return jsonify(res), 200
+
 
 # run the app
 if __name__ == '__main__':
--- detect_faces.py
+++ detect_faces.py
@@ -1,28 +1,101 @@
 #!/usr/bin/env python3
 
+from PIL import Image
+import numpy as np
+import argparse
 import requests
 import base64
-import json
+import glob
+import sys
 import os
 
+# some colors
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+# set up the command-line usage
+ap = argparse.ArgumentParser(description='detect face mask from images')
+ap.add_argument('-i', '--img', help='path to the image to be detected')
+ap.add_argument('-d', '--dir', help='path to the directory containing images')
+ap.add_argument('-o', '--out', default=os.getcwd(),
+                help='output directory or file path')
+ap.ad_argument('-v', '--verbose', action='store_true',
+               help='get a more verbose output')
+
+# add the usage information
+if sys.argv == 1:
+    ap.print_help(sys.stderr)
+    sys.exit(1)
+
+# get the values of the command line arguments
+args = ap.parse_args()
+
+# make the list of images if images are specified
+if not args.dir:
+    imageList = args.img.split()
+    outDir = os.getcwd()
+else:
+    imageList = glob.glob(os.path.sep.join([args.dir, '*']))
+    outDir = args.dir
+
+# src directory
+srcDir = os.getcwd()
+
+# do some primary checks
+for idx, imgs in enumerate(imageList):
+    if imgs.rsplit('.')[-1] not in ['jpg', 'jpeg', 'png']:
+        print(f'{WARNING}[INFO] {imgs} is not an image file, skipping...{ENDC}')
+        imageList.pop(idx)
+
+# get the paths of images if directory not mentioned
+if not args.dir:
+    for idx, imgs in enumerate(imageList):
+        imageList[idx] = os.path.sep.join([srcDir, imgs])
+
 # initialize the image path
-data = '/home/rahi/face mask detection/download/00000000.jpeg'
-# open the image
-# get the image bytes
-with open(data, 'rb') as f:
-    img = base64.b64encode(f.read()).decode('utf-8')
-# get the file name and extension for later use
-fname, ext = data.split(os.sep)[-1].rsplit('.')
+data = 'D://face-mask-detection/dataset/val/iwm/00007_Mask_Mouth_Chin.jpg'
+
+# initialize image data list
+imgData = []
+
+for imgPath in imageList:
+    # open the image
+    # get the image bytes
+    with open(imgPath, 'rb') as f:
+        # encode the image to send it to server
+        if args.verbose:
+            print(f'{OKBLUE} encoding {imgPath}{ENDC}')
+        img = base64.b64encode(f.read()).decode('utf-8')
+    # get the file name and extension for later use
+    fname, ext = data.split(os.sep)[-1].rsplit('.')
+    imgData.append((img, fname, ext))
 
 # prepare the payload
-payload = {'imgB': img, 'ext': ext}
+if args.verbose:
+    print(f'{OKGREEN}[INFO] preparing the payload{ENDC}')
+payload = {'imgB': imgData}
+
+print('sending the request')
 # make the POST request
 r = requests.post("http://127.0.0.1:5000/detect", data=payload)
 
-print('sending the request')
-
 # get the data from response
 res = r.json()
-# get the image bytes
-responseBytes = base64.b64encode(res['detected_faces'].encode('utf-8'))
 
+# get all the images
+for (imgList, fname, ext) in res['detected_faces']:
+    nparr = np.array(imgList, dtype=np.uint8)
+    imgToSave = Image.fromarray(nparr)
+    outFname = f'{fname}_detected_face(s).{ext}'
+    outPath = os.path.sep.join([outDir, outFname])
+    if args.verbose:
+        print(f'{OKBLUE}[INFO] saving the image with detected faces in {outPath}{ENDC}')
+    imgToSave.save(outPath)
+
+print(f'{OKGREEN} All done!!{ENDC}')
